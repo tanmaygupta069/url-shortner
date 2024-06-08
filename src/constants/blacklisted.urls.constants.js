2d033441558d09f60de34d6c91cd76cf9f155e81
tanmay gupta <tanmay3301.69@gmail.com>
2024-06-08 17:40:01 +0530

    fix(constants): optimised getBlackListedUrl

--- src/constants/blacklisted.urls.constants.js
+++ src/constants/blacklisted.urls.constants.js
@@ -3,21 +3,19 @@ const { environments } = require("../constants");
 
 const localBlackListedUrls = ["https://localhost:8080"];
 const devBlackListedUrls = ["https://localhost:8081"];
-const prodBlackListedUrls = ["https://xhamster.com","https://pornhub.com"];
-
+const prodBlackListedUrls = ["https://xhamster.com", "https://pornhub.com"];
 
 const getBlackListedUrls = () => {
   switch (config.server.ENV) {
-    case environments.LOCAL:
-      return localBlackListedUrls;
-    case environments.DEV:
-      return devBlackListedUrls;
     case environments.PROD:
       return prodBlackListedUrls;
+    case environments.DEV:
+      return devBlackListedUrls;
+    case environments.LOCAL:
+      return localBlackListedUrls;
     default:
       return [];
   }
 };
-
 
 module.exports = getBlackListedUrls;
